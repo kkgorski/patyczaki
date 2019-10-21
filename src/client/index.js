7d1918fbbc2a8fb7261fa0203bc871b57946dd2f
kkgorski <jgorski63@gmail.com>
2019-10-21 21:52:39 +0200

    Adding empty canvas

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,6 @@
-window.alert("Hello World!")
+var canvas = document.getElementById("game-canvas");
+canvas.width = 800;
+canvas.height = 800;
+var context = canvas.getContext("2d");
+context.fillStyle = "#CDCDCD";
+context.fillRect(0, 0, 500, 500);
