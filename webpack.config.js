5269a981e04060c5da06ba86ed430c0f3faacf47
kkgorski <jgorski63@gmail.com>
2019-10-22 19:05:17 +0200

    Changing name convention from client/server to front/backend

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/client/index.js',
+  entry: './src/front-end/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
