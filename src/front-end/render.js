73f8ff07d906b0c0f77205ac69588f16bf6d36c1
kkgorski <jgorski63@gmail.com>
2019-11-04 19:11:28 +0100

    Players name are rendered above picture

--- src/front-end/render.js
+++ src/front-end/render.js
@@ -10,7 +10,9 @@ export const updateItems = (gameState) => {
     return {
       x: gameStateItem.x,
       y: gameStateItem.y,
-      img: getAsset('patyczak.png')
+      img: getAsset('patyczak.png'),
+      type: gameStateItem.type,
+      name: gameStateItem.name
     };
   });
 }
@@ -19,7 +21,14 @@ function render() {
   context.fillStyle = "#CDCDCD";
   context.fillRect(0, 0, Const.MAX_WIDTH, Const.MAX_HEIGHT);
   items.forEach((renderable) => {
-    context.drawImage(renderable.img, renderable.x, renderable.y);}
+    context.drawImage(renderable.img, renderable.x, renderable.y);
+    if(renderable.type === Const.TYPE.PLAYER){
+      context.fillStyle = "red";
+      context.font = "14px Arial";
+      context.textBaseline = "middle";
+      context.fillText(renderable.name, renderable.x + (Const.PLAYER_WIDTH / 2), renderable.y + 14);
+    }
+  }
   );
 }
 
